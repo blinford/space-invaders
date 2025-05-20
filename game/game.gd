68efaad6cc3dd7219a49daa30968b2ae33c52959
Benjamin Linford <blinford@live.unc.edu>
2025-05-20 17:06:48 -0400

    adjusted resolution

--- game/game.gd
+++ game/game.gd
@@ -5,6 +5,8 @@ extends Node2D
 
 func _ready() -> void:
 	player.connect("spawn_bullet", _spawn_bullet)
+	player.position.x = get_viewport().size.x/2
+	player.position.y = get_viewport().size.y - 100
 
 func _spawn_bullet(pos: Vector2) -> void:
 	var bullet = bullet_scene.instantiate()
