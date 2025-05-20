60afda8a85991fa2908dbd35fe212956805023da
Benjamin Linford <blinford@live.unc.edu>
2025-05-20 16:57:12 -0400

    fixed bullet position

--- game/game.gd
+++ game/game.gd
@@ -4,10 +4,9 @@ extends Node2D
 @onready var bullet_scene = preload("res://game/bullet/bullet.tscn")
 
 func _ready() -> void:
-	yield(self, "start_game")
 	player.connect("spawn_bullet", _spawn_bullet)
 
 func _spawn_bullet(pos: Vector2) -> void:
 	var bullet = bullet_scene.instantiate()
-	bullet.position = pos
 	add_child(bullet)
+	bullet.global_position = pos
--- main.gd
+++ main.gd
@@ -1,8 +1,18 @@
 extends Node2D
 
+@onready var menu = $Menu
+@onready var game_scene : PackedScene = preload("res://game/game.tscn")
+
+var game = null
+
 func _ready() -> void:
 	pass
 
 func _input(event: InputEvent) -> void:
-	if Input.is_action_pressed("ui_accept"):
+	if Input.is_action_pressed("ui_accept") and game == null:
+		_start_game()
 		
+func _start_game() -> void:
+	menu.hide()
+	game = game_scene.instantiate()
+	add_child(game)
